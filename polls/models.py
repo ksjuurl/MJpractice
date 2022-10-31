2237fd6f517d6f0d7d31d844f8ac1c2cfa361a31
kim su2 <ksj2@naver.com>
2022-11-01 02:42:28 +0900

    DB create : polls/model.py , settings.py 에 추가함. migration 적용.

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)    # 질문내용
+    pub_date = models.DateField('date published')       # 질문생성날짜
+
+    #모든내용표시하기
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)    #  선택지에해당하는 질문
+    choice_text = models.CharField(max_length=200)  # 상세내용    
+    votes = models.IntegerField(default=0)          # 투표수
+
+    #모든내용표시하기
+    def __str__(self):
+        return self.choice_text
+
+
